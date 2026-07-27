f4c25904fc439e4e6f9372c0191f8d9635ba1c11
asaeltapro <asaeltavarezburgos1@gmail.com>
2026-07-27 18:05:25 -0400

    correccion de errore segun fran

--- source/crypto.py
+++ source/crypto.py
@@ -1,11 +1,12 @@
 import hashlib
+import os
 
 def obtener_hash_archivo(ruta_archivo):
     sha1 = hashlib.sha1()
     try:
         with open(ruta_archivo, 'rb') as f:
             while True:
-                data = f.read(4096) 
+                data = f.read(4096)
                 if not data:
                     break
                 sha1.update(data)
@@ -13,19 +14,25 @@ def obtener_hash_archivo(ruta_archivo):
     except FileNotFoundError:
         return None
 
-def generar_hash_tree(lista_de_archivos): 
+def generar_hash_tree(lista_de_rutas):
     componentes = []
-    for nombre, hash_archivo in lista_de_archivos:
-        linea = f"blob {hash_archivo} {nombre}\n"
-        componentes.append(linea)
+    for ruta in lista_de_rutas:
+        hash_actual = obtener_hash_archivo(ruta)
+        nombre_archivo = os.path.basename(ruta)
+        
+        if hash_actual:
+            linea = f"blob {hash_actual} {nombre_archivo}\n"
+            componentes.append(linea)
 
     componentes.sort()
-
     string_plano = "".join(componentes)
     data_en_bytes = string_plano.encode('utf-8')
-
+    
     motor = hashlib.sha1()
-    motor.update(data_en_bytes) 
-
+    motor.update(data_en_bytes)
+    
     return motor.hexdigest()
 
+if __name__ == "__main__":
+    archivos = ["source/crypto.py", "source/storage.py"]
+    print(generar_hash_tree(archivos))
