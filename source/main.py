a36098e275b3cdb59c39bec3dd5a63bf1bb3e5e6
JPujols54 <justinpujols4@gmail.comgit config --global user.email justinpujols4@gmail.comgit config --global user.email justinpujols4@gmail.com>
2026-07-28 22:43:09 -0400

    feat(history.py) agrega la funcion de log

--- source/main.py
+++ source/main.py
@@ -12,7 +12,9 @@ app = typer.Typer(
     add_completion = False
 )
 
-def pre_hash():
+def pre_hash() -> None | str:
+    """Coloca el hash del commit anterior al nuevo como parent"""
+    
     ruta = Path(".minigit/commits")
     lista = sorted(ruta.iterdir())
     
@@ -36,7 +38,7 @@ def init():
 @app.command()
 def add(
     archivo =typer.Argument(None)
-):
+) -> None:
     """Agrega uno o varios archivos para subirlos a tu commit"""
     if archivo:
         if archivo == ".":
@@ -48,11 +50,12 @@ def add(
         elif storage.copy_staging(archivo) != None:
             print(storage.copy_staging(archivo))
 
-
 @app.command()
 def commit(
     message: Annotated[str, typer.Option(...,'-m',"--message", help="Mensaje del commit")]
-):
+) -> None:
+    """Guarda tu commit junto a sus metadatos"""
+
     comentario: str = message
     ruta_index = Path(".minigit/index")
     
@@ -66,5 +69,12 @@ def commit(
     history.move_stagin(rutas_history[0])
     
     history.meta_data(comentario,hash,rutas_history[1],hash_pre)
+
+@app.command()
+def log() -> None:
+    """Imprime en pantalla el contenidos de los metadatos de todos los commits"""
+
+    history.command_log()
+
 if __name__ == "__main__":
     app()
