951b56ecb495f184a3b53fce8b965b2e7486ef13
JPujols54 <justinpujols4@gmail.com>
2026-08-17 14:02:17 -0400

    Mejora en la funcion escribir_bytes

--- source/storage/fs.py
+++ source/storage/fs.py
@@ -1,20 +1,22 @@
 from pathlib import Path
-
+import tempfile
 def crear_directorio(ruta: Path):
     """Crea el directorio si no existe."""
     Path(ruta).mkdir(parents=True, exist_ok=True)
 
-def exists(path: Path) -> bool:
+def exists(path: str) -> bool:
     """Verifica si un archivo o directorio existe."""
     return Path(path).exists()
 
-def leer_byte(ruta: Path):
+def leer_byte(ruta: str):
     verificar = exists(ruta)
     if verificar:
         bytes_content: bytes = Path(ruta).read_bytes()
         return bytes_content
     return None
 
-def escribir_bytes(ruta:Path, datos):
-    Path(ruta).write_bytes(datos)
-    
+def escribir_bytes(ruta: str, datos: bytes):
+    target = Path(ruta)
+    verificar = exists(ruta)
+    if verificar:
+        target.write_bytes(datos)
